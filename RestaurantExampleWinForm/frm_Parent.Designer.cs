2063352676d82b17a375dcc254c7ee5f0a919c63
alimertcetin <mrtmert54@gmail.com>
2022-04-18 23:18:35 +0300

    Initial Commit

---
+++ RestaurantExampleWinForm/frm_Parent.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace RestaurantExampleWinForm
+{
+    partial class frm_Parent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_Parent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 749);
+            this.IsMdiContainer = true;
+            this.Name = "frm_Parent";
+            this.Text = "frm_Parent";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ RestaurantExampleWinForm/frm_Parent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantExampleWinForm
+{
+    public partial class frm_Parent : Form
+    {
+        public frm_Parent()
+        {
+            InitializeComponent();
+        }
+    }
+}
