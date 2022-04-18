1034ece9e89d71fb09a72fae147892f73f08b094
alimertcetin <mrtmert54@gmail.com>
2022-04-19 02:29:19 +0300

    Created Food class to store data

---
+++ RestaurantExampleWinForm/Classes/Food.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Restaurant.Data
+{
+    public enum FoodType
+    {
+        Hamburger = 1,
+        Ingredient = 2,
+    }
+
+    [System.Serializable]
+    public class Food
+    {
+        public string ID { get; private set; }
+        public string Name;
+        public double Price;
+        public FoodType FoodType;
+
+        public Food(string name, double price, FoodType type)
+        {
+            this.ID = Guid.NewGuid().ToString();
+            this.Name = name;
+            this.Price = price;
+            this.FoodType = type;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+}
